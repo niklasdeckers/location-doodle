036a31dfef11b8f446ec766224360e192d2e8616
Niklas Deckers <niklas.deckers@web.de>
2018-08-19 00:33:28 +0200

    Add 6-digit identifier logic

--- backend/php/src/Model/Event.php
+++ backend/php/src/Model/Event.php
@@ -54,7 +54,14 @@ class Event
         $this->displayName = $displayName;
         $this->topic = $topic;
 
-        $this->eventId = '123456'; // TODO: ReplaceMe with logic
+        $random_string_length=6;
+        $characters = 'abcdefghijkmnopqrstuvwxyz023456789';//omitted 1 and l
+        $string = '';
+        $max = strlen($characters) - 1;
+        for ($i = 0; $i < $random_string_length; $i++) {
+            $string .= $characters[mt_rand(0, $max)];
+        }
+        $this->eventId = $string;
     }
 
     /**
@@ -76,6 +83,27 @@ class Event
      */
     public static function getMockedEvent($eventId)
     {
+        $event = new self(
+            Participant::getMockedInvitor(),
+            new \DateTime('+2  Weeks'),
+            new \DateTime('tomorrow'),
+            'Kugel schieben mit den Jungs',
+            'Fussball'
+        );
+        $event->eventId = $eventId;
+
+        return $event;
+    }
+
+    /**
+     * @param string $eventId
+     *
+     * @return Event
+     */
+    public static function getEventFromDB($eventId)
+    {
+
+
         $event = new self(
             Participant::getMockedInvitor(),
             new \DateTime('+2  Weeks'),
