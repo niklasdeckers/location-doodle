1e601a89ce827f75caf257a4370f5e6c58f97af3
Niklas Deckers <niklas.deckers@web.de>
2018-08-18 22:34:53 +0200

    Test GET param access by printing handle object

--- backend/python/httpserver.py
+++ backend/python/httpserver.py
@@ -3,7 +3,7 @@ import http.server
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        DUMMY_RESPONSE="Heureka"
+        DUMMY_RESPONSE=str(self)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.send_header("Content-length", len(DUMMY_RESPONSE))
@@ -15,3 +15,15 @@ def run(server_class=http.server.HTTPServer, handler_class=CustomHandler):
     server_address = ('', 8080)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
+
+
+"""
+
+'''
+@return list of optimal meeting points as "long,lat"
+'''
+def optimal_meeting_points(arrival_time: str, starting_locations: List[str]) -> List[str]:
+   pass
+
+
+"""
