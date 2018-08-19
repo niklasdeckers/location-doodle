15e7c1e5941c071719650abcb8060af790c28690
Niklas Deckers <niklas.deckers@web.de>
2018-08-19 06:27:51 +0200

    Add time mapping logic

--- backend/python/logic.py
+++ backend/python/logic.py
@@ -29,13 +29,13 @@ def bundles_to_json_responses(bundles):
     responses = get_multiple_async([generate_single_api_url(**bundle) for bundle in bundles])
     return [decode_single_api_response(each) for each in responses]
 
-def generate_bundle_set(arrival_time, starting_location, steps_iter=range(5,90,10)):
+def generate_bundle_set(arrival_time, starting_location, steps_iter=range(5,90,5)):
     return [{"start_time":arrival_time-datetime.timedelta(minutes=step),"max_time":step,"starting_location":starting_location} for step in steps_iter]
 
-def bundle_iterator(responses,lens):
+def bundle_iterator(zipped_responses,lens):
     pos=0
     for each in lens:
-        yield responses[pos:pos+each]
+        yield zipped_responses[pos:pos+each]
         pos+=each
 
 def join_split_bundle_sets(bundle_sets):
@@ -43,22 +43,56 @@ def join_split_bundle_sets(bundle_sets):
     bundles=itertools.chain.from_iterable(bundle_sets)
     responses=bundles_to_json_responses(bundles)
     #print(responses)
-    return bundle_iterator(responses,lens)
+    return bundle_iterator(zip(responses,bundles),lens)
 
-def process_single_bundle_set_results(results):
-    print(results)
-    #TODO
+def process_single_bundle_set_results(results,arrival_time):
+    """
+
+    alle zeitschritte durchgehen. böse zeit ist kleinstes startzeit-endzeit, wobei nicht nach endzeit ankommen darf
+
+
+    :param results:
+    :return:
+    """
+    total_mapping={}
+    for result,bundle in results:
+        current_mapping={v["Stn"]["id"]: bundle["start_time"] for v in result["Res"]["Isochrone"]["IsoDest"] if bundle["start_time"]+datetime.timedelta(minutes=int(''.join(c for c in v["duration"] if c.isdigit())))<=arrival_time}
+        for k,v in current_mapping:
+            if k in total_mapping:
+                total_mapping[k]=min(total_mapping[k],current_mapping[k])
+            else:
+                total_mapping[k]=current_mapping[k]
+    return total_mapping
+
+def sum_maps(maps):
+    pass
 
 def optimal_meeting_points(arrival_time, starting_locations):
-    arrival_time= dateutil.parser.parse(arrival_time)
+    arrival_time = dateutil.parser.parse(arrival_time)
     #todo #print([generate_bundle_set(arrival_time,starting_location) for starting_location in starting_locations])
     res=join_split_bundle_sets([generate_bundle_set(arrival_time,starting_location) for starting_location in starting_locations])
+    maps=[]
     for each in res:
         try:
             each[0]["Res"]["Isochrone"]
-            process_single_bundle_set_results(each)
+            single_map=process_single_bundle_set_results(each,arrival_time)
+            maps.append(single_map)
         except:
             pass
             # todo bei der berechnung nicht einbeziehen
+    if len(maps)==0:
+        return #TODO dann auftraggeber-Startpunkt verwenden?
+    else:
+        summed=sum_maps(maps)#todo weighted sum
+
+
+
+        #todo apply weight function for best picks
+
+
+
+
+
+
 
     return []
